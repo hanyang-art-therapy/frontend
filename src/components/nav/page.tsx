e1c56a7d7aba4e434c3d37b7c135eab822d91958
daeundan <ekdms534@naver.com>
2025-04-30 17:16:06 +0900

    chore-scss-setting

---
+++ src/components/nav/page.tsx
@@ -0,0 +1,32 @@
+import '../../styles/nav.scss';
+
+export default function Nav() {
+  return (
+    <nav>
+      <ul className='icon-wrap'>
+        <li>
+          <a href='#'>로고자리</a>
+        </li>
+
+        <li>
+          <a href='#'>학과소개</a>
+        </li>
+        <li>
+          <a href='#'>임상활동</a>
+        </li>
+        <li>
+          <a href='#'>ART+THERAPY 展</a>
+        </li>
+        <li>
+          <a href='#'>입학 안내</a>
+        </li>
+        <li>
+          <a href='#'>자유 게시판</a>
+        </li>
+        <li>
+          <a href='#'>돋보기</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
