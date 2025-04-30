79dbd1ba40b922a6b9e3c30cee75cd82088c8477
daeundan <ekdms534@naver.com>
2025-04-30 17:16:06 +0900

    chore-scss-setting

---
+++ src/components/header/header.tsx
@@ -0,0 +1,22 @@
+// 코드가 너무 길어지는 것 이외에도 책임 분리가 필요하면 top-header, bottom-header 2개의 컴포넌트로 분리
+import '../../styles/header.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href='#'>사이트맵</a>
+        </li>
+        <div className='btn-wrap'>
+          <li>
+            <a href='#'>로그인</a>
+          </li>
+          <li>
+            <a href='#'>마이페이지</a>
+          </li>
+        </div>
+      </ul>
+    </header>
+  );
+}
