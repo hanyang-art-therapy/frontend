6ca42cb0af835e4aeebf0066ee658833516599a8
Kled <1017yu@daum.net>
2025-05-01 11:21:31 +0900

    refactor: `cn` 사용으로 변경

--- src/components/ui/step.tsx
+++ src/components/ui/step.tsx
@@ -1,4 +1,5 @@
 import type { Dispatch, SetStateAction } from 'react';
+import { cn } from '@/lib/utils';
 
 type StepProps = {
   items: string[];
@@ -13,9 +14,10 @@ export default function Step({ items, step, setStep }: StepProps) {
         {items.map((item, index) => (
           <li
             key={index}
-            className={`w-1/3 border-l border-r border-gray-300 first:border-r-0 last:border-l-0 hover:bg-orange-100 hover:text-primary transition-all duration-300 ${
-              step === item ? 'bg-orange-100 text-primary' : ''
-            }`}>
+            className={cn(
+              'w-1/3 border-l border-r border-gray-300 first:border-r-0 last:border-l-0 hover:bg-orange-100 hover:text-primary transition-all duration-300',
+              step === item && 'bg-orange-100 text-primary'
+            )}>
             <button
               onClick={() => setStep(item)}
               className='w-full h-full flex items-center justify-center cursor-pointer'>
