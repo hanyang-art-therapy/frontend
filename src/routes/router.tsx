087d192fcdf696b4842fc4aa4186a1c4f51ccc6b
daeundan <ekdms534@naver.com>
2025-05-04 01:56:31 +0900

    design: artdetail 작품설명UI 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -6,7 +6,7 @@ import SigninPage from '@/pages/signin/page';
 import MyPage from '@/pages/my-page/page';
 import Home from '@/pages/page';
 import { createBrowserRouter } from 'react-router-dom';
-import ArtsDetail from '@/components/gallery/arts/artsdetail.tsx/artsdetail';
+import ArtsDetail from '@/components/gallery/arts/artsdetail copy';
 
 const router = createBrowserRouter([
   {
@@ -20,9 +20,9 @@ const router = createBrowserRouter([
         path: '/gallery',
         element: <GalleryPage />,
       },
-      // 갤러리 아이디 연결하여 작품설명 화면으로 넘어가게 설정하였습니다
+      // id가 아닌 artsNo으로 바꿈
       {
-        path: '/gallery/:id',
+        path: '/gallery/:artsNo',
         element: <ArtsDetail />,
       },
       {
