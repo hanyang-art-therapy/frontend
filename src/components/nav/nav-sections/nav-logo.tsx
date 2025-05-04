02bae55b6baeee94ad0d2b14d7714fa2520bc752
Kled <1017yu@daum.net>
2025-05-05 00:50:15 +0900

    feat: 내비게이션 메뉴 상수화 진행

--- src/components/nav/nav-sections/nav-logo.tsx
+++ src/components/nav/nav-sections/nav-logo.tsx
@@ -1,15 +1,19 @@
-export default function Logo() {
-    return (
-      <>
-        <h1>
-          <a href="/">
-            <img
-              src="/images/logo.webp"
-              alt="한양대학교 에리카 미술치료학과"
-              className="w-[302px] min-w-[220px] h-[30px] object-contain"
-            />
-          </a>
-        </h1>
-      </>
-    );
-  }
+type LogoProps = {
+  subName?: string;
+};
+
+export default function Logo({ subName }: LogoProps) {
+  return (
+    <h1>
+      <a href='/'>
+        <img
+          src='/images/logo.webp'
+          alt='한양대학교 에리카 미술치료학과'
+          className='min-w-[220px] h-[30px] object-contain'
+        />
+
+        {subName && <p className='text-r-14'>{subName}</p>}
+      </a>
+    </h1>
+  );
+}
--- src/components/nav/nav-sections/nav-menu.tsx
+++ src/components/nav/nav-sections/nav-menu.tsx
@@ -1,77 +1,68 @@
-import HamburgerButton from '@/components/nav/nav-sections/nav-menu-btn/hemburger-button';
-import { useState } from 'react';
+import HamburgerButton from '@/components/nav/nav-sections/nav-menu-btn/hamburger-button';
+import { NAV_MENU } from '@/constants/nav';
 import { Search } from 'lucide-react';
-import '@/tw-styles.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavMenu() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isSlideOpen, setIsSlideOpen] = useState(false);
 
   return (
     <>
       {/* 데스크탑 메뉴 */}
-      <div className="hidden md:flex items-center gap-[50px] title-m-18">
-        <ul className="flex gap-[30px] relative">
-          <li className="relative group">
-            <p className="main-menu">학과 소개</p>
-            <ul className="sub-menu text-r-16 group-hover:block">
-                <li><a href="#">미래상</a></li>
-                <li><a href="#">교수진</a></li>
-                <li><a href="#">교육 과정</a></li>
-                <li><a href="#">자격증</a></li>
-                <li><a href="#">졸업 후 전망</a></li>
-                <li><a href="#">MOU기관</a></li>
-                <li><a href="#">오시는 길</a></li>
-            </ul>
-          </li>
-          <li className="relative group">
-            <p className="main-menu">임상활동</p>
-            <ul className="sub-menu text-r-16 group-hover:block">
-                <li><a href="#">발달장애</a></li>
-                <li><a href="#">유아</a></li>
-                <li><a href="#">아동</a></li>
-                <li><a href="#">청소년</a></li>
-                <li><a href="#">성인</a></li>
-                <li><a href="#">노인</a></li>
-            </ul>
-          </li>
-          <li className="relative group">
-            <p className="main-menu">ART+THERAPY 展</p>
-            <ul className="sub-menu text-r-16 group-hover:block">
-                <li><a href="#">전시 소개</a></li>
-                <li><a href="#">미술관 미술치료</a></li>
-                <li><a href="/gallery">갤러리</a></li>
-            </ul>
-          </li>
-          <li className="relative group">
-            <p className="main-menu">입학 안내</p>
-            <ul className="sub-menu text-r-16 group-hover:block">
-                <li><a href="#">장학금 혜택</a></li>
-                <li><a href="#">신입학생 모집</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#" className="main-menu">자유 게시판</a>
-          </li>
+      <div className='hidden md:flex items-center gap-[50px] title-m-18'>
+        <ul className='flex gap-[30px] relative'>
+          {NAV_MENU.map((menu, index) => (
+            <li key={index} className='relative group'>
+              {menu.submenu.length > 0 ? (
+                <>
+                  <Link to={menu.path} className='main-menu'>
+                    {menu.title}
+                  </Link>
+                  <ul className='sub-menu text-r-16 group-hover:block'>
+                    {menu.submenu.map((submenu, subIndex) => (
+                      <li key={subIndex}>
+                        <a href={submenu.path}>{submenu.title}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              ) : (
+                <a href={menu.path} className='main-menu'>
+                  {menu.title}
+                </a>
+              )}
+            </li>
+          ))}
         </ul>
-        <a href="#" className="block">
-          <Search className="w-[24px] h-[24px] text-[#333] md:hover:text-[var(--white)]" />
-        </a>
+
+        <Link to='#' className='block'>
+          <Search className='w-[24px] h-[24px] text-black md:hover:text-white' />
+        </Link>
       </div>
 
       {/* 모바일 메뉴 버튼 */}
-      <div className="flex items-center gap-4 md:hidden">
-        <a href="#"><Search className="w-[24px] h-[24px]" /></a>
-        <HamburgerButton onClick={() => setIsOpen(!isOpen)} aria-label="모바일 메뉴 열기" />
+      <div className='flex items-center gap-4 md:hidden'>
+        <a href='#'>
+          <Search className='w-[24px] h-[24px]' />
+        </a>
+        <HamburgerButton
+          onClick={() => setIsSlideOpen(!isSlideOpen)}
+          aria-label='모바일 메뉴 열기'
+        />
       </div>
 
       {/* 슬라이드 다운 */}
-      <div className={`menu-wrapper absolute top-full left-0 w-full bg-[var(--white)] text-[var(--black)] px-8 py-8 md:hidden z-50 ${isOpen ? 'open' : ''}`}>
-        <ul className="flex flex-col gap-4 text-[18px]">
-          <li><a href="#">학과 소개</a></li>
-          <li><a href="#">임상활동</a></li>
-          <li><a href="/gallery">ART+THERAPY 展</a></li>
-          <li><a href="#">입학 안내</a></li>
-          <li><a href="#">자유 게시판</a></li>
+      <div
+        className={`menu-wrapper absolute top-full left-0 w-full bg-white text-black px-8 py-8 md:hidden z-50 ${
+          isSlideOpen ? 'open' : ''
+        }`}>
+        <ul className='flex flex-col gap-4 text-[18px]'>
+          {NAV_MENU.map((menu, index) => (
+            <li key={index}>
+              <Link to={menu.path}>{menu.title}</Link>
+            </li>
+          ))}
         </ul>
       </div>
     </>
