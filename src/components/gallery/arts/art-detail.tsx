d7e1b6f001e4417f64556e10b3013eadfad2e944
daeundan <ekdms534@naver.com>
2025-05-12 01:56:27 +0900

    design: 캡션 디자인 수정 및 UI 조정

--- src/components/gallery/arts/art-detail.tsx
+++ src/components/gallery/arts/art-detail.tsx
@@ -26,7 +26,7 @@ export default function ArtsDetail() {
         <div className='w-full h-[102px] flex items-center py-[20px]'>
           <ul className='w-full flex justify-between md:text-[24px] font-semibold'>
             <div className='flex gap-[16px]'>
-              <li>9999.12.31</li>
+              <li>2025.05.17</li>
               <li>{art.artTitle}</li>
             </div>
             <li className='flex gap-[16px]'>
@@ -42,14 +42,14 @@ export default function ArtsDetail() {
               alt={art.artistName}
               className='w-full md:w-[720px] py-[10px]'
             />
-            <span className='self-end px-[180px] pb-[30px] text=[#5A5A5A]'>
+            <span className='self-end md:px-[180px] pb-[30px] text=[#5A5A5A]'>
               Acrylic on canvas 100x80cm
             </span>
             <div className='flex md:max-w-[1080px] h-[auto] p-[20px] flex-col items-start gap-[10px] border border-[#ddd] mb-[100px]'>
               <h2 className='w-full md:text-[24px] font-bold pb-[20px] text-left'>
                 작품 설명
               </h2>
-              <div className='text-black md:text-[20px]  text-left leading-[3.5] p-[10px]'>
+              <div className='text-black md:text-[20px] text-left leading-[3.5] p-[10px]'>
                 {art.artDetail}
               </div>
             </div>
