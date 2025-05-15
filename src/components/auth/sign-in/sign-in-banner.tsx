3d8090968d40ff45ceae97b6e51b5c3517baf470
Kled <1017yu@daum.net>
2025-05-15 22:27:25 +0900

    temp

--- src/components/auth/sign-in/sign-in-banner.tsx
+++ src/components/auth/sign-in/sign-in-banner.tsx
@@ -1,13 +1,15 @@
 export default function SignInBanner() {
   return (
-    <div className='bg-bg-gray-fa mt-1 items-center justify-center flex w-screen'>
-      <div className='flex flex-col py-[30px] px-5 md:px-0 md:py-[60px] md:max-w-[770px]'>
+    <div className='bg-bg-gray-fa mt-1 items-center justify-center flex w-screen '>
+      <div className='flex flex-col py-[30px] px-[20px] xl:px-0 md:py-[60px] md:max-w-[770px]'>
         <span className='t-b-32'>한양대학교 에리카</span>
         <p>
-          <span className='t-b-52'>미술치료학과 홈페이지</span>
+          <span className='t-sign-in-banner-highlight'>
+            미술치료학과 홈페이지
+          </span>
           <span className='t-b-32'>에 오신 것을 환영합니다.</span>
         </p>
-        <span className='md:mt-[30px] mt-[15px] t-b-24 text-center opacity-70'>
+        <span className='md:mt-[30px] mt-[15px] t-sign-in-banner-description text-center opacity-70'>
           Hanyang University ERICA Art Therapy Home Page Service
         </span>
       </div>
