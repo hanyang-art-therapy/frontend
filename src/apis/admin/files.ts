25a615c84dd4ec474863cdb7e2d1f6f87b3e22b4
ijikim <bbo05081@gmail.com>
2025-06-08 15:47:16 +0900

    docs: Files API 파일 수정

--- src/apis/admin/files.ts
+++ src/apis/admin/files.ts
@@ -3,6 +3,10 @@ import apiInstance from '@/lib/axios';
 export const FileUpload = async (
   formData: FormData
 ): Promise<{ filesNo: number }> => {
-  const res = await apiInstance.post('/files/upload', formData);
+  const res = await apiInstance.post('/files', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
   return res.data;
 };
